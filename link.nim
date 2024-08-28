4f284c5cf91b3087e6fa0645eb52d0611a2e5987
nirokay <nirokay-public@protonmail.com>
2024-08-28 10:55:57 +0200

    added interactivity and updated script to do everything automatically

--- link.nim
+++ link.nim
@@ -1,16 +1,66 @@
 import std/[os, strutils, strformat, terminal]
 
+# Directories: ----------------------------------------------------------------
+
+var
+    pathHome: string
+    pathRoot: string = "/"
+
+if paramCount() < 1:
+    styledEcho fgRed, "Please provide path to user home as $1 and optionally path to the root of the filesystem as $2"
+    quit 2
+else:
+    pathHome = paramStr(1)
+    if paramCount() >= 2: pathRoot = paramStr(2)
+
+if not pathHome.dirExists() or pathHome == "":
+    styledEcho fgRed, &"User directory '{pathHome}' does not exist."
+    quit 1
+if not pathRoot.dirExists() or pathRoot == "":
+    styledEcho fgRed, &"Root directory '{pathRoot}' does not exist."
+    quit 1
+
+
+# Consent: --------------------------------------------------------------------
+
+styledEcho fgDefault, &"User directory is set to ", fgYellow, pathHome, fgDefault
+styledEcho fgDefault, &"Root directory is set to ", fgYellow, pathRoot, fgDefault
+
+stdout.write "Please verify if those are correct [y/N] "
+case getch():
+    of 'Y', 'y':
+        echo "\nContinuing..."
+    of 'N', 'n':
+        echo "\nAborting..."
+        quit 1
+    else:
+        echo "\nUnclear input, aborting..."
+        quit 1
+
+
+# Start linking process: ------------------------------------------------------
+
 const defaultIndent: int = 2
 
 type Symlink* = object
     src*, dest*: string
-proc `$`*(symlink: Symlink): string =
-    result = &"'{symlink.src}' -> '{symlink.dest}'"
 proc newSymlink*(src, dest: string): Symlink = Symlink(
     src: src,
     dest: dest
 )
-proc createSymlink*(symlink: Symlink) = createSymlink(symlink.src, symlink.dest)
+
+proc `$`*(symlink: Symlink): string =
+    result = &"'{symlink.src}' -> '{symlink.dest}'"
+
+proc createSymlink*(symlink: Symlink, needsSudo: bool = false) = createSymlink(symlink.src, symlink.dest)
+    #[
+    let
+        command: string = (if needsSudo: "sudo " else: "") & &"""ln "{symlink.src}" "{symlink.dest}" """
+        status: int = execShellCmd(command)
+    echo command
+    if status != 0:
+        raise OSError.newException("Failed to create file (missing permissions?)")
+    ]#
 proc symlinkExists*(symlink: Symlink): bool = symlinkExists(symlink.dest)
 
 var failedSymlinks: seq[Symlink]
@@ -45,7 +95,7 @@ proc linkFilesInDirectory*(src: string, dest: string, sudoPrivileges: bool = fal
                     symlink.dest.removeFile()
                 except OSError as e:
                     let msg: string = e.msg.replace("\n", " - ")
-                    styledEcho fgRed, &"\n{indentation}! [Error] Could not remove old symlink '{symlink.dest}' ({msg})", fgDefault
+                    styledEcho fgRed, &"{indentation}! [Error] Could not remove old symlink '{symlink.dest}' ({msg})", fgDefault
             else:
                 styledEcho fgCyan, " [Creating new symlink]", fgDefault
             # Create symlink:
@@ -60,8 +110,8 @@ proc linkFilesInDirectory*(src: string, dest: string, sudoPrivileges: bool = fal
         linkFilesInDirectory(src / path, dest / path, true, indent + defaultIndent)
 
 
-linkFilesInDirectory("home", getHomeDir()) ## Files for `/*`
-linkFilesInDirectory("root", "/", true)    ## Files for `/home/$USER/*`
+linkFilesInDirectory("home", pathHome, false) ## Files for `/*`
+linkFilesInDirectory("root", pathRoot, true)  ## Files for `/home/$USER/*`
 
 if failedSymlinks.len() != 0:
     styledEcho fgRed, "\nFailed to create these symlinks:", fgDefault
--- link.sh
+++ link.sh
@@ -1,61 +1,7 @@
 #!/usr/bin/env bash
 
-shopt -s dotglob
-FAILED_LIST=()
+nim c link.nim &> compile.log
 
-function link_files_of_dir() {
-    SEARCH_DIRECTORY=$1 # ./home/*
-    TARGET_DIRECTORY=$2
-    INDENT=$3
-    [ -z "$INDENT" ] && INDENT="-"
-    echo -e "Linking files in '$SEARCH_DIRECTORY' to '$TARGET_DIRECTORY'!"
-    [ ! -d "$SEARCH_DIRECTORY" ] && {
-        echo -e "Creating directory '$SEARCH_DIRECTORY'"
-        mkdir "$SEARCH_DIRECTORY" || echo -e "Failed to create directory '$SEARCH_DIRECTORY'"
-    }
-    for FILE in "$SEARCH_DIRECTORY"/*; do
-        # Recursion:
-        [ -d "$FILE" ] && {
-            echo -e "link_files_of_dir $FILE $TARGET_DIRECTORY/$(basename "$FILE") $INDENT-"
-            link_files_of_dir "$FILE" "$TARGET_DIRECTORY/$(basename "$FILE")" "$INDENT-"
-        }
-        # Avoid empty directories:
-        [[ "$FILE" == *"*" ]] && {
-            echo -e "Empty directory $FILE"
-            continue
-        }
-        DOTFILE="$TARGET_DIRECTORY/$(basename "$FILE")"
+[ -f "link" ] && ./link $*
+[ ! -f "link" ] && echo -e "Binary could not be found..."
 
-        # Existing target:
-        [ -d "$DOTFILE" ] && {
-            echo -e "Directory with name '$DOTFILE' exists, cannot create symlink)"
-            continue
-        }
-        [ -f "$DOTFILE" ] && {
-            echo -e "File '$DOTFILE' already exists ($(file "$DOTFILE"))"
-            if test -L "$DOTFILE"; then
-                #echo -e "Removing symlink '$DOTFILE'"
-                if ! mv "$DOTFILE" "$HOME/.local/share/Trash/files/$(basename "$DOTFILE")_deleted_by_dotfiles"; then
-                    rm "$DOTFILE"
-                fi
-            else
-                FAILED_LIST+=( "$FILE" )
-                continue
-            fi
-        }
-
-        # Linking fun:
-        echo -e "$INDENT Creating symlink: '$DOTFILE' -> '$FILE'"
-        # ln -s "$FILE" "$DOTFILE"
-    done
-}
-
-PWD=$(pwd)
-
-link_files_of_dir "$PWD/home"   "$HOME"
-# link_files_of_dir "$PWD/config" "$HOME/.config"
-
-[ -z "${FAILED_LIST[*]}" ] || {
-    echo -e "\nFailed to link these files:\n${FAILED_LIST[*]}"
-    exit 1
-}
