41ad86d84ef6e5f36a424eaf1b9003b3f1da7bd4
nirokay <nirokay-public@protonmail.com>
2024-08-30 09:04:12 +0200

    minimal restructuring and beautifying of code

--- link.nim
+++ link.nim
@@ -1,6 +1,6 @@
 import std/[os, strutils, strformat, terminal]
 
-# Directories: ----------------------------------------------------------------
+# Directories: ================================================================
 
 var
     pathHome: string
@@ -21,7 +21,7 @@ if not pathRoot.dirExists() or pathRoot == "":
     quit 1
 
 
-# Consent: --------------------------------------------------------------------
+# Consent: ====================================================================
 
 styledEcho fgDefault, &"User directory is set to ", fgYellow, pathHome, fgDefault
 styledEcho fgDefault, &"Root directory is set to ", fgYellow, pathRoot, fgDefault
@@ -38,37 +38,29 @@ else:
     quit 1
 
 
-# Start linking process: ------------------------------------------------------
+# Start linking process: ======================================================
 
-const defaultIndent: int = 2
 
 type Symlink* = object
     src*, dest*: string
+proc `$`*(symlink: Symlink): string =
+    result = &"'{symlink.src}' -> '{symlink.dest}'"
 proc newSymlink*(src, dest: string): Symlink = Symlink(
     src: src,
     dest: dest
 )
 
-proc `$`*(symlink: Symlink): string =
-    result = &"'{symlink.src}' -> '{symlink.dest}'"
-
 proc createSymlink*(symlink: Symlink, needsSudo: bool = false) = createSymlink(symlink.src, symlink.dest)
-    #[
-    let
-        command: string = (if needsSudo: "sudo " else: "") & &"""ln "{symlink.src}" "{symlink.dest}" """
-        status: int = execShellCmd(command)
-    echo command
-    if status != 0:
-        raise OSError.newException("Failed to create file (missing permissions?)")
-    ]#
 proc symlinkExists*(symlink: Symlink): bool = symlinkExists(symlink.dest)
 
+const defaultIndent: int = 2
 var failedSymlinks: seq[Symlink]
 
 proc linkFilesInDirectory*(src: string, dest: string, sudoPrivileges: bool = false, indent: int = defaultIndent) =
     let indentation: string = repeat('-', indent)
     var dirs: seq[string]
 
+    # Create missing directories: ---------------------------------------------
     styledEcho fgYellow, &"{indentation}/ '{src}' -> '{dest}'", fgDefault
     if not dest.dirExists():
         try:
@@ -78,15 +70,15 @@ proc linkFilesInDirectory*(src: string, dest: string, sudoPrivileges: bool = fal
             let msg: string = e.msg.replace("\n", " - ")
             styledEcho fgRed, &"{indentation}! [Error] Could not create directory: {src} ({msg})", fgDefault
 
-    # Walk over directory, link files, handle subdirectories later:
+    # Walk over directory, link files, handle subdirectories later: -----------
     for kind, path in walkDir(src, true):
-        # Directory:
+        # Directory:  - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         if kind in [pcDir, pcLinkToDir]:
             dirs.add path
             continue
-        # File:
+        # File: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         let symlink: Symlink = newSymlink(absolutePath(src / path), dest / path)
-        # Remove file, if consented:
+        # Remove file, if consented:  - - - - - - - - - - - - - - - - - - - - -
         if symlink.dest.fileExists and not symlink.dest.symlinkExists():
             stdout.styledWrite fgRed, &"{indentation}? File '{symlink.dest}' exists, replace file with symlink? [y/N] ", fgDefault
             case getch():
@@ -100,7 +92,7 @@ proc linkFilesInDirectory*(src: string, dest: string, sudoPrivileges: bool = fal
                 break
         stdout.styledWrite fgDefault, &"{indentation}> Creating symlink: {symlink}", fgDefault
         try:
-            # Remove old symlink, if exists:
+            # Remove old symlink, if exists:  - - - - - - - - - - - - - - - - -
             if symlink.dest.symlinkExists():
                 try:
                     styledEcho fgMagenta, " [Replaing old symlink]", fgDefault
@@ -110,14 +102,14 @@ proc linkFilesInDirectory*(src: string, dest: string, sudoPrivileges: bool = fal
                     styledEcho fgRed, &"{indentation}! [Error] Could not remove old symlink '{symlink.dest}' ({msg})", fgDefault
             else:
                 styledEcho fgCyan, " [Creating new symlink]", fgDefault
-            # Create symlink:
+            # Create symlink: - - - - - - - - - - - - - - - - - - - - - - - - -
             symlink.createSymlink()
         except OSError as e:
             let msg: string = e.msg.replace("\n", " - ")
             styledEcho fgRed, &"{indentation}! [Error] Could not create symlink: {symlink} ({msg})", fgDefault
             failedSymlinks.add symlink
 
-    # Subdirectories:
+    # Subdirectories: - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     for path in dirs:
         linkFilesInDirectory(src / path, dest / path, true, indent + defaultIndent)
 
